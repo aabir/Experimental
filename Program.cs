bfeabdb6205824790c399cb1ad9354828471eb96
Shible Chowdhury <Shible.Chowdhury@theaccessgroup.com>
2022-04-21 23:00:50 +0800

    Added Serilogs for logging.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using PostGreBE.Data;
 using PostGreBE.Entity;
+using Serilog;
+using Serilog.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,30 +24,55 @@ namespace PostGreBE
 
         public async static Task Main(string[] args)
         {
-            var host = CreateHostBuilder(args)
-                         .Build();
-            using (var scope = host.Services.CreateScope())
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            try
             {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-                try
+                Log.Information("Application Starting.");
+
+                var host = CreateHostBuilder(args)
+                         .Build();
+                using (var scope = host.Services.CreateScope())
                 {
-                    //Seed Default Users
-                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                    await ApplicationDbContextSeed.SeedEssentialsAsync(userManager, roleManager);
+                    var services = scope.ServiceProvider;
+                    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                    try
+                    {
+                        //Seed Default Users
+                        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                        await ApplicationDbContextSeed.SeedEssentialsAsync(userManager, roleManager);
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = loggerFactory.CreateLogger<Program>();
+                        logger.LogError(ex, "An error occurred seeding the DB.");
+                    }
                 }
-                catch (Exception ex)
-                {
-                    var logger = loggerFactory.CreateLogger<Program>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
+                host.Run();
+
             }
-            host.Run();
+            catch(Exception ex)
+            {
+                Log.Fatal(ex, "The application failed to start.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
